a362f1183d14e7386ccff35197d09f5d1b6c5628
00hz <742693072@qq.com>
2021-01-05 11:00:05 +0800

    update engine files

--- common/engine.cpp
+++ common/engine.cpp
@@ -20,9 +20,6 @@
 
 using namespace nvinfer1;
 
-
-RTEngine::RTEngine() {}
-
 RTEngine::~RTEngine() {
     if (mPluginFactory != nullptr) {
         delete mPluginFactory;
@@ -41,35 +38,6 @@ RTEngine::~RTEngine() {
     }
 }
 
-void RTEngine::CreateEngine(const std::string& prototxt,
-                            const std::string& caffeModel,
-                            const std::string& engineFile,
-                            const std::vector<std::string>& outputBlobName,
-                            const std::vector<std::vector<float>>& calibratorData,
-                            int maxBatchSize,
-                            int mode) {
-    mRunMode = mode;
-    mInfoLogger.logger("prototxt: ",prototxt);
-    mInfoLogger.logger("caffeModel: ",caffeModel);
-    mInfoLogger.logger("engineFile: ",engineFile);
-    mInfoLogger.logger("outputBlobName: ");
-    for (size_t i=0;i<outputBlobName.size();i++) {
-        std::cout << outputBlobName[i] << " ";
-    }
-    std::cout << std::endl;
-    if (!DeserializeEngine(engineFile)) {
-        if (!BuildEngine(prototxt,caffeModel,engineFile,outputBlobName,calibratorData,maxBatchSize)) {
-            mInfoLogger.logger("error: could not deserialize or build engine");
-            return;
-        }
-    }
-    mInfoLogger.logger("Create execute context and malloc device memory...");
-    mFromOnnx = false;
-    InitEngine();
-    // Notice: close profiler
-    //mContext->setProfiler(mProfiler);
-}
-
 void RTEngine::CreateEngine(const std::string& onnxModel,
                             const std::string& engineFile,
                             const std::vector<std::string>& customOutput,
@@ -86,66 +54,12 @@ void RTEngine::CreateEngine(const std::string& onnxModel,
     InitEngine();
 }
 
-void RTEngine::CreateEngine(const std::string& uffModel,
-                            const std::string& engineFile,
-                            const std::vector<std::string>& inputTensorNames,
-                            const std::vector<std::vector<int>>& inputDims,
-                            const std::vector<std::string>& outputTensorNames,
-                            int maxBatchSize) {
-    if (!DeserializeEngine(engineFile)) {
-        if (!BuildEngine(uffModel,engineFile,inputTensorNames,inputDims, outputTensorNames,maxBatchSize)) {
-            mInfoLogger.logger("could not deserialize or build engine", logger::LEVEL::ERROR);
-            return;
-        }
-    }
-    mInfoLogger.logger("create execute context and malloc device memory...");
-    InitEngine();
-}
-
 void RTEngine::Forward() {
-//     cudaEvent_t start,stop;
-//     float elapsedTime;
-//     cudaEventCreate(&start);
-//     cudaEventCreate(&stop);
-//     cudaEventRecord(start, 0);
     mContext->execute(mBatchSize, &mBinding[0]);
-//     cudaEventRecord(stop, 0);
-//	 cudaEventSynchronize(stop);
-//	 cudaEventElapsedTime(&elapsedTime, start, stop);
-//     mInfoLogger.logger("net forward takes: ", elapsedTime, "ms");
 }
 
 void RTEngine::ForwardAsync(const cudaStream_t& stream) {
-//    cudaEvent_t start,stop;
-//    float elapsedTime;
-//    cudaEventCreate(&start);
-//    cudaEventCreate(&stop);
-//    cudaEventRecord(start, 0);
     mContext->enqueue(mBatchSize, &mBinding[0], stream, nullptr);
-//    cudaEventRecord(stop, 0);
-//    cudaEventSynchronize(stop);
-//    cudaEventElapsedTime(&elapsedTime, start, stop);
-//    mInfoLogger.logger("net forward takes: ", elapsedTime, "ms");
-}
-
-void RTEngine::DataTransfer(std::vector<float>& data, int bindIndex, bool isHostToDevice) {
-    if (isHostToDevice) {
-        assert(data.size()*sizeof(float) == mBindingSize[bindIndex]);
-        CUDA_CHECK(cudaMemcpy(mBinding[bindIndex], data.data(), mBindingSize[bindIndex], cudaMemcpyHostToDevice));
-    } else {
-        data.resize(mBindingSize[bindIndex]/sizeof(float));
-        CUDA_CHECK(cudaMemcpy(data.data(), mBinding[bindIndex], mBindingSize[bindIndex], cudaMemcpyDeviceToHost));
-    }
-}
-
-void RTEngine::DataTransferAsync(std::vector<float>& data, int bindIndex, bool isHostToDevice, cudaStream_t& stream) {
-    if (isHostToDevice) {
-        assert(data.size()*sizeof(float) == mBindingSize[bindIndex]);
-        CUDA_CHECK(cudaMemcpyAsync(mBinding[bindIndex], data.data(), mBindingSize[bindIndex], cudaMemcpyHostToDevice, stream));
-    } else {
-        data.resize(mBindingSize[bindIndex]/sizeof(float));
-        CUDA_CHECK(cudaMemcpyAsync(data.data(), mBinding[bindIndex], mBindingSize[bindIndex], cudaMemcpyDeviceToHost, stream));
-    }
 }
 
 void RTEngine::CopyFromHostToDevice(const std::vector<float>& input, int bindIndex) {
@@ -170,7 +84,7 @@ void RTEngine::SetDevice(int device) {
 }
 
 int RTEngine::GetDevice() {
-    int* device = nullptr; //NOTE: memory leaks here
+    int* device = nullptr;  // NOTE: memory leaks here
     CUDA_CHECK(cudaGetDevice(device));
     if (device != nullptr) {
         return device[0];
@@ -245,121 +159,6 @@ bool RTEngine::DeserializeEngine(const std::string& engineFile) {
     return false;
 }
 
-bool RTEngine::BuildEngine(const std::string& prototxt,
-                           const std::string& caffeModel,
-                           const std::string& engineFile,
-                           const std::vector<std::string>& outputBlobName,
-                           const std::vector<std::vector<float>>& calibratorData,
-                           int maxBatchSize) {
-        mBatchSize = maxBatchSize;
-        mInfoLogger.logger("build caffe engine with: ", prototxt, caffeModel);
-        nvinfer1::IBuilder* builder = nvinfer1::createInferBuilder(mLogger);
-        assert(builder != nullptr);
-        // NetworkDefinitionCreationFlag::kEXPLICIT_BATCH
-        nvinfer1::INetworkDefinition* network = builder->createNetworkV2(0);
-        assert(network != nullptr);
-        nvcaffeparser1::ICaffeParser* parser = nvcaffeparser1::createCaffeParser();
-        if (mPluginFactory != nullptr) {
-//            parser->setPluginFactoryV2(mPluginFactory);
-        }
-        // Notice: change here to costom data type
-        nvinfer1::DataType type = mRunMode==1 ? nvinfer1::DataType::kHALF : nvinfer1::DataType::kFLOAT;
-        const nvcaffeparser1::IBlobNameToTensor* blobNameToTensor = parser->parse(prototxt.c_str(),caffeModel.c_str(),
-                                                                                *network,type);
-
-        for (auto& s : outputBlobName) {
-            network->markOutput(*blobNameToTensor->find(s.c_str()));
-        }
-        mInfoLogger.logger("Number of network layers: ",network->getNbLayers());
-        mInfoLogger.logger("Number of input: ", network->getNbInputs());
-        std::cout << "Input layer: " << std::endl;
-        for (int i = 0; i < network->getNbInputs(); i++) {
-            std::cout << network->getInput(i)->getName() << " : ";
-            Dims dims = network->getInput(i)->getDimensions();
-            for (int j = 0; j < dims.nbDims; j++) {
-                std::cout << dims.d[j] << "x";
-            }
-            std::cout << "\b "  << std::endl;
-        }
-        mInfoLogger.logger("Number of output: ",network->getNbOutputs());
-        std::cout << "Output layer: " << std::endl;
-        for (int i = 0; i < network->getNbOutputs(); i++) {
-            std::cout << network->getOutput(i)->getName() << " : ";
-            Dims dims = network->getOutput(i)->getDimensions();
-            for(int j = 0; j < dims.nbDims; j++) {
-                std::cout << dims.d[j] << "x";
-            }
-            std::cout << "\b " << std::endl;
-        }
-        mInfoLogger.logger("parse network done");
-        nvinfer1::IBuilderConfig* config = builder->createBuilderConfig();
-
-        IInt8EntropyCalibrator* calibrator = nullptr;
-        if (mRunMode == 2)
-        {
-            mInfoLogger.logger("set int8 inference mode");
-            if (!builder->platformHasFastInt8()) {
-                mInfoLogger.logger("current platform doesn't support int8 inference", logger::LEVEL::WARNING);
-            }
-            if (calibratorData.size() > 0 ) {
-                auto endPos= prototxt.find_last_of(".");
-                auto beginPos= prototxt.find_last_of('/') + 1;
-                if(prototxt.find("/") == std::string::npos) {
-                    beginPos = 0;
-                }
-                std::string calibratorName = prototxt.substr(beginPos,endPos - beginPos);
-                std::cout << "create calibrator,Named:" << calibratorName << std::endl;
-//                calibrator = new IInt8EntropyCalibrator(maxBatchSize,calibratorData,calibratorName,false);
-            }
-            // enum class BuilderFlag : int
-            // {
-            //     kFP16 = 0,         //!< Enable FP16 layer selection.
-            //     kINT8 = 1,         //!< Enable Int8 layer selection.
-            //     kDEBUG = 2,        //!< Enable debugging of layers via synchronizing after every layer.
-            //     kGPU_FALLBACK = 3, //!< Enable layers marked to execute on GPU if layer cannot execute on DLA.
-            //     kSTRICT_TYPES = 4, //!< Enables strict type constraints.
-            //     kREFIT = 5,        //!< Enable building a refittable engine.
-            // };
-            config->setFlag(nvinfer1::BuilderFlag::kINT8);
-            config->setInt8Calibrator(calibrator);
-        }
-
-        if (mRunMode == 1)
-        {
-            mInfoLogger.logger("setFp16Mode");
-            if (!builder->platformHasFastFp16()) {
-                mInfoLogger.logger("the platform do not has fast for fp16");
-            }
-            config->setFlag(nvinfer1::BuilderFlag::kFP16);
-        }
-        builder->setMaxBatchSize(mBatchSize);
-        // set the maximum GPU temporary memory which the engine can use at execution time.
-        config->setMaxWorkspaceSize(10 << 20);
-
-        mInfoLogger.logger("fp16 support: ",builder->platformHasFastFp16 ());
-        mInfoLogger.logger("int8 support: ",builder->platformHasFastInt8 ());
-        mInfoLogger.logger("Max batchsize: ",builder->getMaxBatchSize());
-        mInfoLogger.logger("Max workspace size: ",config->getMaxWorkspaceSize());
-        mInfoLogger.logger("Number of DLA core: ",builder->getNbDLACores());
-        mInfoLogger.logger("Max DLA batchsize: ",builder->getMaxDLABatchSize());
-        mInfoLogger.logger("Current use DLA core: ",config->getDLACore()); // TODO: set DLA core
-        mInfoLogger.logger("build engine...");
-        mEngine = builder -> buildEngineWithConfig(*network, *config);
-        assert(mEngine != nullptr);
-        mInfoLogger.logger("serialize engine to: ", engineFile);
-        SaveEngine(engineFile);
-
-        builder->destroy();
-        config->destroy();
-        network->destroy();
-        parser->destroy();
-        if (calibrator) {
-            delete calibrator;
-            calibrator = nullptr;
-        }
-        return true;
-}
-
 bool RTEngine::BuildEngine(const std::string& onnxModel,
                            const std::string& engineFile,
                            const std::vector<std::string>& customOutput,
@@ -379,38 +178,7 @@ bool RTEngine::BuildEngine(const std::string& onnxModel,
         mInfoLogger.logger("Could not parse onnx engine", logger::LEVEL::ERROR);
         return false;
     }
-//    for(int i=0;i<network->getNbLayers();i++) {
-//        nvinfer1::ILayer* custom_output = network->getLayer(i);
-//        for(int j=0;j<custom_output->getNbInputs();j++) {
-//            nvinfer1::ITensor* input_tensor = custom_output->getInput(j);
-//            std::cout << input_tensor->getName() << " ";
-//        }
-//        std::cout << " -------> ";
-//        for(int j=0;j<custom_output->getNbOutputs();j++) {
-//            nvinfer1::ITensor* output_tensor = custom_output->getOutput(j);
-//            std::cout << output_tensor->getName() << " ";
-//        }
-//        std::cout << std::endl;
-//    }
-//    if(customOutput.size() > 0) {
-//        mInfoLogger.logger("unmark original output...");
-//        for(int i=0;i<network->getNbOutputs();i++) {
-//            nvinfer1::ITensor* origin_output = network->getOutput(i);
-//            network->unmarkOutput(*origin_output);
-//        }
-//        mInfoLogger.logger("mark custom output...");
-//        for(int i=0;i<network->getNbLayers();i++) {
-//            nvinfer1::ILayer* custom_output = network->getLayer(i);
-//            nvinfer1::ITensor* output_tensor = custom_output->getOutput(0);
-//            for(size_t j=0; j<customOutput.size();j++) {
-//                std::string layer_name(output_tensor->getName());
-//                if(layer_name == customOutput[j]) {
-//                    network->markOutput(*output_tensor);
-//                    break;
-//                }
-//            }
-//        }
-//    }
+
     nvinfer1::IBuilderConfig* config = builder->createBuilderConfig();
     builder->setMaxBatchSize(mBatchSize);
     config->setMaxWorkspaceSize(workspace_size << 20);
@@ -451,70 +219,6 @@ bool RTEngine::BuildEngine(const std::string& onnxModel,
     parser->destroy();
     return true;
 }
-
-bool RTEngine::BuildEngine(const std::string& uffModel,
-                           const std::string& engineFile,
-                           const std::vector<std::string>& inputTensorNames,
-                           const std::vector<std::vector<int>>& inputDims,
-                           const std::vector<std::string>& outputTensorNames,
-                           int maxBatchSize) {
-    mBatchSize = maxBatchSize;
-    mInfoLogger.logger("build uff engine with: ", uffModel);
-    nvinfer1::IBuilder* builder = nvinfer1::createInferBuilder(mLogger);
-    assert(builder != nullptr);
-    // NetworkDefinitionCreationFlag::kEXPLICIT_BATCH
-    nvinfer1::INetworkDefinition* network = builder->createNetworkV2(0);
-    assert(network != nullptr);
-    nvuffparser::IUffParser* parser = nvuffparser::createUffParser();
-    assert(parser != nullptr);
-    assert(inputTensorNames.size() == inputDims.size());
-    //parse input
-    for (size_t i=0;i<inputTensorNames.size();i++) {
-        nvinfer1::Dims dim;
-        dim.nbDims = inputDims[i].size();
-        for(int j=0;j<dim.nbDims;j++) {
-            dim.d[j] = inputDims[i][j];
-        }
-        parser->registerInput(inputTensorNames[i].c_str(), dim, nvuffparser::UffInputOrder::kNCHW);
-    }
-    //parse output
-    for (size_t i=0;i<outputTensorNames.size();i++) {
-        parser->registerOutput(outputTensorNames[i].c_str());
-    }
-    if (!parser->parse(uffModel.c_str(), *network, nvinfer1::DataType::kFLOAT)) {
-        mInfoLogger.logger("parse model failed", logger::LEVEL::ERROR);
-    }
-    nvinfer1::IBuilderConfig* config = builder->createBuilderConfig();
-    config->setMaxWorkspaceSize(10 << 20);
-    builder->setMaxBatchSize(mBatchSize);
-    mEngine = builder -> buildEngineWithConfig(*network, *config);
-    assert(mEngine != nullptr);
-    mInfoLogger.logger("serialize engine to: ", engineFile);
-    SaveEngine(engineFile);
-
-    builder->destroy();
-    network->destroy();
-    parser->destroy();
-    return true;
-}
-
-// void RTEngine::ParseUff(const std::string& uffModel) {
-//     mInfoLogger.logger("parse uff model, this parser is very simple");
-//     mInfoLogger.logger("I recommend you to see https://lutzroeder.github.io/netron/");
-//     mInfoLogger.logger("You will get a more user-friendly output");
-//     tensorflow::GraphDef graph;
-//     fstream input(uffModel.c_str(), ios::in | ios::binary);
-//     if (!graph.ParseFromIstream(&input)) {
-//         mInfoLogger.logger("Failed to parse uff model");
-//         return;
-//     }
-//     for(int i=0;i<graph.node_size();i++) {
-//         tensorflow::NodeDef node = graph.node(i);
-//         std::cout << "--------------------------------" << std::endl;
-//         std::cout << node.name() << ", " << node.op() << std::endl;
-//     }
-
-// }
 
 void RTEngine::InitEngine() {
     mInfoLogger.logger("Init engine...");
--- common/engine.h
+++ common/engine.h
@@ -16,8 +16,6 @@
 #include "logger.h"
 #include "utils.h"
 
-
-
 class RTEngineLogger : public nvinfer1::ILogger {
     void log(Severity severity, const char* msg) override
     {
@@ -31,41 +29,19 @@ class RTEngineLogger : public nvinfer1::ILogger {
 class RTEngine {
 public:
     /**
-     * @description: default constructor, will initialize plugin factory with default parameters.
+     * Default constructor.
      */
-    RTEngine();
+    RTEngine() = default;
     ~RTEngine();
 
     /**
-     * @description: create engine from caffe prototxt and caffe model
-     * @prototxt: caffe prototxt
-     * @caffemodel: caffe model contain network parameters
-     * @engineFile: serialzed engine file, if it does not exit, will build engine from
-     *              prototxt and caffe model, which take about 1 minites, otherwise will
-     *              deserialize enfine from engine file, which is very fast.
-     * @outputBlobName: specify which layer is network output, find it in caffe prototxt
-     * @calibratorData: use for int8 mode, calabrator data is a batch of sample input,
-     *                  for classification task you need around 500 sample input. and this
-     *                  is for int8 mode
-     * @maxBatchSize: max batch size while inference, make sure it do not exceed max batch
-     *                size in your model
-     * @mode: engine run mode, 0 for float32, 1 for float16, 2 for int8
-     */
-    void CreateEngine(const std::string& prototxt,
-                      const std::string& caffeModel,
-                      const std::string& engineFile,
-                      const std::vector<std::string>& outputBlobName,
-                      const std::vector<std::vector<float>>& calibratorData,
-                      int maxBatchSize,
-                      int mode);
-
-    /**
-     * @description: create engine from onnx model
-     * @onnxModel: path to onnx model
-     * @engineFile: path to saved engien file will be load or save, if it's empty them will not
+     * Create engine from onnx model.
+     * onnxModel: path to onnx model
+     * engineFile: path to saved engien file will be load or save, if it's empty them will not
      *              save engine file
-     * @maxBatchSize: max batch size for inference.
-     * @return:
+     * maxBatchSize: max batch size for inference.
+     * runMode: mode while running, fp32/fp16/int8
+     * return:
      */
     void CreateEngine(const std::string& onnxModel,
                       const std::string& engineFile,
@@ -75,50 +51,16 @@ public:
                       long workspace_size);
 
     /**
-     * @description: create engine from uff model
-     * @uffModel: path to uff model
-     * @engineFile: path to saved engien file will be load or save, if it's empty them will not
-     *              save engine file
-     * @inputTensorName: input tensor
-     * @outputTensorName: output tensor
-     * @maxBatchSize: max batch size for inference.
-     * @return:
-     */
-    void CreateEngine(const std::string& uffModel,
-                      const std::string& engineFile,
-                      const std::vector<std::string>& inputTensorName,
-                      const std::vector<std::vector<int>>& inputDims,
-                      const std::vector<std::string>& outputTensorName,
-                      int maxBatchSize);
-
-    /**
-     * @description: do inference on engine context, make sure you already copy your data to device memory,
-     *               see DataTransfer and CopyFromHostToDevice etc.
+     * Do inference on engine context, make sure you already copy your data to device memory,
+     * using CopyFromHostToDevice etc.
      */
     void Forward();
 
     /**
-     * @description: async inference on engine context
-     * @stream: cuda stream for async inference and data transfer
+     * Async inference on engine context.
+     * stream: cuda stream for async inference and data copy
      */
     void ForwardAsync(const cudaStream_t& stream);
-
-    /**
-     * @description: data transfer between host and device, for example befor Forward, you need
-     *               copy input data from host to device, and after Forward, you need to transfer
-     *               output result from device to host.
-     * @data: data for read and write.
-     * @bindIndex: binding data index, you can see this in CreateEngine log output.
-     * @isHostToDevice: 0 for device to host, 1 for host to device (host: cpu memory, device: gpu memory)
-     */
-    void DataTransfer(std::vector<float>& data, int bindIndex, bool isHostToDevice);
-
-    /**
-     * @description: async data tranfer between host and device, see above.
-     * @stream: cuda stream for async interface and data transfer.
-     * @return:
-     */
-    void DataTransferAsync(std::vector<float>& data, int bindIndex, bool isHostToDevice, cudaStream_t& stream);
 
     void CopyFromHostToDevice(const std::vector<float>& input, int bindIndex);
 
@@ -133,50 +75,42 @@ public:
     int GetDevice();
 
     /**
-     * @description: get max batch size of build engine.
-     * @return: max batch size of build engine.
+     * Get max batch size of build engine.
+     * return: max batch size of build engine.
      */
     int GetMaxBatchSize() const;
 
     /**
-     * @description: get binding data pointer in device. for example if you want to do some post processing
-     *               on inference output but want to process them in gpu directly for efficiency, you can
-     *               use this function to avoid extra data io
-     * @return: pointer point to device memory.
+     * Get binding data pointer in device. For example if you want to do some post processing
+     * on inference output but want to process them in gpu directly for efficiency, you can
+     * use this function to avoid extra data io.
+     * return: pointer point to device memory.
      */
     void* GetBindingPtr(int bindIndex) const;
 
     /**
-     * @description: get binding data size in byte, so maybe you need to divide it by sizeof(T) where T is data type
-     *               like float.
-     * @return: size in byte.
+     * Get binding data size in byte, so maybe you need to divide it by sizeof(T) where T is data type
+     * like float.
+     * return: size in byte.
      */
     size_t GetBindingSize(int bindIndex) const;
 
     /**
-     * @description: get binding dimemsions
-     * @return: binding dimemsions, see https://docs.nvidia.com/deeplearning/sdk/tensorrt-api/c_api/classnvinfer1_1_1_dims.html
+     * Get binding dimemsions.
+     * return: binding dimemsions, see https://docs.nvidia.com/deeplearning/sdk/tensorrt-api/c_api/classnvinfer1_1_1_dims.html
      */
     nvinfer1::Dims GetBindingDims(int bindIndex) const;
 
     /**
-     * @description: get binding data type
-     * @return: binding data type, see https://docs.nvidia.com/deeplearning/sdk/tensorrt-api/c_api/namespacenvinfer1.html#afec8200293dc7ed40aca48a763592217
+     * Get binding data type.
+     * return: binding data type, see https://docs.nvidia.com/deeplearning/sdk/tensorrt-api/c_api/namespacenvinfer1.html#afec8200293dc7ed40aca48a763592217
      */
     nvinfer1::DataType GetBindingDataType(int bindIndex) const;
 
     std::vector<std::string> mBindingName;
 
-protected:
-
+private:
     bool DeserializeEngine(const std::string& engineFile);
-
-    bool BuildEngine(const std::string& prototxt,
-                     const std::string& caffeModel,
-                     const std::string& engineFile,
-                     const std::vector<std::string>& outputBlobName,
-                     const std::vector<std::vector<float>>& calibratorData,
-                     int maxBatchSize);
 
     bool BuildEngine(const std::string& onnxModel,
                      const std::string& engineFile,
@@ -185,26 +119,17 @@ protected:
                      RunMode runMode,
                      long workspace_size);
 
-    bool BuildEngine(const std::string& uffModel,
-                     const std::string& engineFile,
-                     const std::vector<std::string>& inputTensorName,
-                     const std::vector<std::vector<int>>& inputDims,
-                     const std::vector<std::string>& outputTensorName,
-                     int maxBatchSize);
-
-    // void ParseUff(const std::string& uffModel);
-
     /**
-     * description: Init resource such as device memory
+     * Init resource such as device memory
      */
     void InitEngine();
 
     /**
-     * description: save engine to engine file
+     * Save engine to engine file
      */
     void SaveEngine(const std::string& fileName);
 
-protected:
+private:
     RTEngineLogger mLogger;
     logger::Logger mInfoLogger;
 
@@ -228,8 +153,6 @@ protected:
     std::vector<nvinfer1::DataType> mBindingDataType;
 
     int mInputSize = 0;
-
-    // batch size
     int mBatchSize;
     bool mFromOnnx = true;
 };
