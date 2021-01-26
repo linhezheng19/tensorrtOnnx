cdf374d84ea0b7ea1e4a780b0ee7816a074ac535
00hz <742693072@qq.com>
2021-01-26 18:33:03 +0800

    add video test code

--- main.cpp
+++ main.cpp
@@ -126,124 +126,285 @@ int main(){
     {
 /* -==================classification task================*/
         if (cls){
-            for (size_t i = 0; i < count; i++)
-            {
-                cv::Mat frame = imread(cls_cfg["inputs"]["img_path"].as<string>());
-                int im_w = cls_cfg["inputs"]["width"].as<int>();
-                int im_h = cls_cfg["inputs"]["height"].as<int>();
-                cv::resize(frame, frame, cv::Size(im_w, im_h));
-                int batch_size = cls_cfg["engine"]["bchw"].as<vector<int>>()[0];
-                vector<cv::Mat> imgs;
-                for(int i = 0; i < batch_size; i++){
-                    imgs.emplace_back(frame);
+            int im_w = cls_cfg["inputs"]["width"].as<int>();
+            int im_h = cls_cfg["inputs"]["height"].as<int>();
+            int batch_size = cls_cfg["engine"]["bchw"].as<vector<int>>()[0];
+            string video_path = cls_cfg["inputs"]["video_path"].as<string>();
+            if (!video_path.empty()) {
+                cv::VideoCapture video;
+                cv::Mat frame;
+                frame = video.open(video_path);
+                long total_frames = static_cast<long>(video.get(CAP_PROP_FRAME_COUNT));
+                if (!video.isOpened()) {
+                    cerr << "Video is not opened!" << endl;
+                    cerr << "Please check the video path in *.yaml" << endl;
+                    return -1;
                 }
-                auto cls_results = cls->run(imgs);
+                int skip_frames = 0;
+                for (int i = 0; i < skip_frames; ++i) {
+                    video.read(frame);
+                }
+                for (int i = 0; i < 10; i += batch_size) {
+                    vector <cv::Mat> imgs;
+                    for (int b = 0; b < batch_size; ++b) {
+                        video.read(frame);
+                        cv::resize(frame, frame, cv::Size(im_w, im_h));
+                        imgs.emplace_back(frame);
+                    }
+                    auto cls_results = cls->run(imgs);
+                }
+
+            } else {
+                for (size_t i = 0; i < count; i++) {
+                    vector <cv::Mat> imgs;
+                    for (int i = 0; i < batch_size; i++) {
+                        cv::Mat frame = imread(cls_cfg["inputs"]["img_path"].as<string>());
+                        cv::resize(frame, frame, cv::Size(im_w, im_h));
+                        imgs.emplace_back(frame);
+                    }
+                    auto cls_results = cls->run(imgs);
+                }
             }
             delete cls;
             cls = nullptr;
         }
 /* -==================segmentation task================*/
         if (semseg){
-            for (size_t i = 0; i < count; i++)
-            {
-                cv::Mat frame = imread(semseg_cfg["inputs"]["img_path"].as<string>());
-                int im_w = semseg_cfg["inputs"]["width"].as<int>();
-                int im_h = semseg_cfg["inputs"]["height"].as<int>();
-                cv::resize(frame, frame, cv::Size(im_w, im_h));
-                int batch_size = semseg_cfg["engine"]["bchw"].as<vector<int>>()[0];
-                vector<cv::Mat> imgs;
-                for(int i = 0; i < batch_size; i++){
-                    imgs.emplace_back(frame);
+            int im_w = semseg_cfg["inputs"]["width"].as<int>();
+            int im_h = semseg_cfg["inputs"]["height"].as<int>();
+            int batch_size = semseg_cfg["engine"]["bchw"].as<vector<int>>()[0];
+            string video_path = semseg_cfg["inputs"]["video_path"].as<string>();
+            if (!video_path.empty()) {
+                cv::VideoCapture video;
+                cv::Mat frame;
+                frame = video.open(video_path);
+                long total_frames = static_cast<long>(video.get(CAP_PROP_FRAME_COUNT));
+                if (!video.isOpened()) {
+                    cerr << "Video is not opened!" << endl;
+                    cerr << "Please check the video path in *.yaml" << endl;
+                    return -1;
                 }
-                auto semseg_results = semseg->run(imgs);
+                int skip_frames = 0;
+                for (int i = 0; i < skip_frames; ++i) {
+                    video.read(frame);
+                }
+                for (int i = 0; i < 10; i += batch_size) {
+                    vector <cv::Mat> imgs;
+                    for (int b = 0; b < batch_size; ++b) {
+                        video.read(frame);
+                        cv::resize(frame, frame, cv::Size(im_w, im_h));
+                        imgs.emplace_back(frame);
+                    }
+                    auto semseg_results = semseg->run(imgs);
+                }
+
+            } else {
+                for (size_t i = 0; i < count; i++) {
+                    vector <cv::Mat> imgs;
+                    for (int i = 0; i < batch_size; i++) {
+                        cv::Mat frame = imread(semseg_cfg["inputs"]["img_path"].as<string>());
+                        cv::resize(frame, frame, cv::Size(im_w, im_h));
+                        imgs.emplace_back(frame);
+                    }
+                    auto semseg_results = semseg->run(imgs);
+                }
             }
             delete semseg;
             semseg = nullptr;
         }
-/* -==================track task================*/
-        // fairmot
-        if (fairmot){
-            for (size_t i = 0; i < count; i++)
-            {
-                cv::Mat frame = imread(fairmot_cfg["inputs"]["img_path"].as<string>());
-                int im_w = fairmot_cfg["inputs"]["width"].as<int>();
-                int im_h = fairmot_cfg["inputs"]["height"].as<int>();
-                cv::resize(frame, frame, cv::Size(im_w, im_h));
-                int batch_size = fairmot_cfg["engine"]["bchw"].as<vector<int>>()[0];
-                vector<cv::Mat> imgs;
-                for(int i = 0; i < batch_size; i++){
-                    imgs.emplace_back(frame);
-                }
-                auto fairmot_results = fairmot->run(imgs);
-            }
-            delete fairmot;
-            fairmot = nullptr;
-        }    
-        // f-track
-        if (f_track){
-            for (size_t i = 0; i < count; i++)
-            {
-                cv::Mat frame = imread(f_track_cfg["inputs"]["img_path"].as<string>());
-                int im_w = f_track_cfg["inputs"]["width"].as<int>();
-                int im_h = f_track_cfg["inputs"]["height"].as<int>();
-                cv::resize(frame, frame, cv::Size(im_w, im_h));
-                int batch_size = f_track_cfg["engine"]["bchw"].as<vector<int>>()[0];
-                vector<cv::Mat> imgs;
-                for(int i = 0; i < batch_size; i++){
-                    imgs.emplace_back(frame);
-                }
-                // auto start = chrono::system_clock::now();
-                auto f_track_results = f_track->run(imgs);
-                // auto end = chrono::system_clock::now();
-                // auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
-                // cout << "Infer Timer : " << duration.count() << "ms" << endl;
-            }
-            delete f_track;
-            f_track = nullptr;
-        }
 /* -==================detection task================*/
         // fcos
         if (fcos){
-            for (size_t i = 0; i < count; i++)
-            {
-                cv::Mat frame = imread(fcos_cfg["inputs"]["img_path"].as<string>());
-                int im_w = fcos_cfg["inputs"]["width"].as<int>();
-                int im_h = fcos_cfg["inputs"]["height"].as<int>();
-                cv::resize(frame, frame, cv::Size(im_w, im_h));
-                int batch_size = fcos_cfg["engine"]["bchw"].as<vector<int>>()[0];
-                vector<cv::Mat> imgs;
-                for(int i = 0; i < batch_size; i++){
-                    imgs.emplace_back(frame);
+            int im_w = fcos_cfg["inputs"]["width"].as<int>();
+            int im_h = fcos_cfg["inputs"]["height"].as<int>();
+            int batch_size = fcos_cfg["engine"]["bchw"].as<vector<int>>()[0];
+            string video_path = fcos_cfg["inputs"]["video_path"].as<string>();
+            if (!video_path.empty()) {
+                cv::VideoCapture video;
+                cv::Mat frame;
+                frame = video.open(video_path);
+                long total_frames = static_cast<long>(video.get(CAP_PROP_FRAME_COUNT));
+                if (!video.isOpened()) {
+                    cerr << "Video is not opened!" << endl;
+                    cerr << "Please check the video path in *.yaml" << endl;
+                    return -1;
                 }
-                // auto start = chrono::system_clock::now();
-                auto fcos_results = fcos->run(imgs);
-                // auto end = chrono::system_clock::now();
-                // auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
-                // cout << "Infer Timer : " << duration.count() << "ms" << endl;
-                // vis_detection(fcos_results, imgs);
-                // cv::imwrite("../data/fcos_results.jpg", imgs[0]);
+                int skip_frames = 0;
+                for (int i = 0; i < skip_frames; ++i) {
+                    video.read(frame);
+                }
+                for (int i = 0; i < 10; i += batch_size) {
+                    vector <cv::Mat> imgs;
+                    for (int b = 0; b < batch_size; ++b) {
+                        video.read(frame);
+                        cv::resize(frame, frame, cv::Size(im_w, im_h));
+                        imgs.emplace_back(frame);
+                    }
+                    auto fcos_results = fcos->run(imgs);
+                }
+
+            } else {
+                for (size_t i = 0; i < count; i++) {
+                    cv::Mat frame = imread(fcos_cfg["inputs"]["img_path"].as<string>());
+                    int im_w = fcos_cfg["inputs"]["width"].as<int>();
+                    int im_h = fcos_cfg["inputs"]["height"].as<int>();
+                    cv::resize(frame, frame, cv::Size(im_w, im_h));
+                    int batch_size = fcos_cfg["engine"]["bchw"].as < vector < int >> ()[0];
+                    vector <cv::Mat> imgs;
+                    for (int i = 0; i < batch_size; i++) {
+                        imgs.emplace_back(frame);
+                    }
+                    // auto start = chrono::system_clock::now();
+                    auto fcos_results = fcos->run(imgs);
+                    // auto end = chrono::system_clock::now();
+                    // auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
+                    // cout << "Infer Timer : " << duration.count() << "ms" << endl;
+                    // vis_detection(fcos_results, imgs);
+                    // cv::imwrite("../data/fcos_results.jpg", imgs[0]);
+                }
             }
             delete fcos;
             fcos = nullptr;
         }
         // yolo
         if (yolo){
-            for (size_t i = 0; i < count; i++)
-            {
-                cv::Mat frame = imread(yolo_cfg["inputs"]["img_path"].as<string>());
-                int im_w = yolo_cfg["inputs"]["width"].as<int>();
-                int im_h = yolo_cfg["inputs"]["height"].as<int>();
-                int batch_size = yolo_cfg["engine"]["bchw"].as<vector<int>>()[0];
-                vector<cv::Mat> imgs;
-                for(int i = 0; i < batch_size; i++){
-                    imgs.emplace_back(frame);
+            int im_w = yolo_cfg["inputs"]["width"].as<int>();
+            int im_h = yolo_cfg["inputs"]["height"].as<int>();
+            int batch_size = yolo_cfg["engine"]["bchw"].as<vector<int>>()[0];
+            string video_path = yolo_cfg["inputs"]["video_path"].as<string>();
+            if (!video_path.empty()) {
+                cv::VideoCapture video;
+                cv::Mat frame;
+                frame = video.open(video_path);
+                long total_frames = static_cast<long>(video.get(CV_CAP_PROP_FRAME_COUNT));
+                if (!video.isOpened()) {
+                    cerr << "Video is not opened!" << endl;
+                    cerr << "Please check the video path in *.yaml" << endl;
+                    return -1;
                 }
-                auto yolo_results = yolo->run(imgs);
-                vis_detection(yolo_results, imgs);
-                cv::imwrite("../data/yolo_results.jpg", imgs[0]);
+                int skip_frames = 0;
+                for (int i = 0; i < skip_frames; ++i) {
+                    video.read(frame);
+                }
+                for (int i = 0; i < 10; i += batch_size) {
+                    vector <cv::Mat> imgs;
+                    for (int b = 0; b < batch_size; ++b) {
+                        video.read(frame);
+                        cv::resize(frame, frame, cv::Size(im_w, im_h));
+                        imgs.emplace_back(frame);
+                    }
+                    auto yolo_results = yolo->run(imgs);
+                }
+
+            } else {
+                for (size_t i = 0; i < count; i++)
+                {
+                    vector<cv::Mat> imgs;
+                    for(int i = 0; i < batch_size; i++){
+                        cv::Mat frame = imread(yolo_cfg["inputs"]["img_path"].as<string>());
+                        cv::resize(frame, frame, cv::Size(im_w, im_h));
+                        imgs.emplace_back(frame);
+                    }
+                    auto yolo_results = yolo->run(imgs);
+                    vis_detection(yolo_results, imgs);
+                    cv::imwrite("../data/yolo_results.jpg", imgs[0]);
+                }
             }
             delete yolo;
             yolo = nullptr;
+        }
+/* -==================track task================*/
+        // fairmot
+        if (fairmot){
+            int im_w = fairmot_cfg["inputs"]["width"].as<int>();
+            int im_h = fairmot_cfg["inputs"]["height"].as<int>();
+            int batch_size = fairmot_cfg["engine"]["bchw"].as<vector<int>>()[0];
+            string video_path = fairmot_cfg["inputs"]["video_path"].as<string>();
+            if (!video_path.empty()) {
+                cv::VideoCapture video;
+                cv::Mat frame;
+                frame = video.open(video_path);
+                long total_frames = static_cast<long>(video.get(CAP_PROP_FRAME_COUNT));
+                if (!video.isOpened()) {
+                    cerr << "Video is not opened!" << endl;
+                    cerr << "Please check the video path in *.yaml" << endl;
+                    return -1;
+                }
+                int skip_frames = 0;
+                for (int i = 0; i < skip_frames; ++i) {
+                    video.read(frame);
+                }
+                for (int i = 0; i < 10; i += batch_size) {
+                    vector <cv::Mat> imgs;
+                    for (int b = 0; b < batch_size; ++b) {
+                        video.read(frame);
+                        cv::resize(frame, frame, cv::Size(im_w, im_h));
+                        imgs.emplace_back(frame);
+                    }
+                    auto fairmot_results = fairmot->run(imgs);
+                }
+
+            } else {
+                for (size_t i = 0; i < count; i++) {
+                    vector <cv::Mat> imgs;
+                    for (int i = 0; i < batch_size; i++) {
+                        cv::Mat frame = imread(fairmot_cfg["inputs"]["img_path"].as<string>());
+                        cv::resize(frame, frame, cv::Size(im_w, im_h));
+                        imgs.emplace_back(frame);
+                    }
+                    auto fairmot_results = fairmot->run(imgs);
+                }
+            }
+            delete fairmot;
+            fairmot = nullptr;
+        }
+        // f-track
+        if (f_track){
+            int im_w = f_track_cfg["inputs"]["width"].as<int>();
+            int im_h = f_track_cfg["inputs"]["height"].as<int>();
+            int batch_size = f_track_cfg["engine"]["bchw"].as<vector<int>>()[0];
+            string video_path = f_track_cfg["inputs"]["video_path"].as<string>();
+            if (!video_path.empty()) {
+                cv::VideoCapture video;
+                cv::Mat frame;
+                frame = video.open(video_path);
+                long total_frames = static_cast<long>(video.get(CAP_PROP_FRAME_COUNT));
+                if (!video.isOpened()) {
+                    cerr << "Video is not opened!" << endl;
+                    cerr << "Please check the video path in *.yaml" << endl;
+                    return -1;
+                }
+                int skip_frames = 0;
+                for (int i = 0; i < skip_frames; ++i) {
+                    video.read(frame);
+                }
+                for (int i = 0; i < 10; i += batch_size) {
+                    vector <cv::Mat> imgs;
+                    for (int b = 0; b < batch_size; ++b) {
+                        video.read(frame);
+                        cv::resize(frame, frame, cv::Size(im_w, im_h));
+                        imgs.emplace_back(frame);
+                    }
+                    auto f_track_results = f_track->run(imgs);
+                }
+
+            } else {
+                for (size_t i = 0; i < count; i++) {
+                    vector <cv::Mat> imgs;
+                    for (int i = 0; i < batch_size; i++) {
+                        cv::Mat frame = imread(f_track_cfg["inputs"]["img_path"].as<string>());
+                        cv::resize(frame, frame, cv::Size(im_w, im_h));
+                        imgs.emplace_back(frame);
+                    }
+                    // auto start = chrono::system_clock::now();
+                    auto f_track_results = f_track->run(imgs);
+                    // auto end = chrono::system_clock::now();
+                    // auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
+                    // cout << "Infer Timer : " << duration.count() << "ms" << endl;
+                }
+            }
+            delete f_track;
+            f_track = nullptr;
         }
     }
     cout << "DONE!\n";
